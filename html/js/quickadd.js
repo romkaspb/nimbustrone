ec0a3eee8fcccb8457459b2e3d0f8805f2ab966b
Roman Novoselov <rnovoselov93@gmail.com>
2017-05-21 23:45:28 +0300

    removed unused vars, comments and added traymenu

--- html/js/quickadd.js
+++ html/js/quickadd.js
@@ -1,6 +1,8 @@
-var ipcRenderer = require('electron').ipcRenderer;
+var ipcRenderer = require('electron').ipcRenderer
+const remote = require('electron').remote
 const quickAddForm = document.getElementById('quickadd-form')
 
 quickAddForm.addEventListener('submit', function (event) {
-  ipcRenderer.send('create-new-note', document.getElementById('quickadd').value)
+  ipcRenderer.send('create-text-note', document.getElementById('quickadd').value)
+  remote.getCurrentWindow().hide()
 })
--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const electron = require('electron');
-const app = electron.app;  // Модуль контролирующей жизненный цикл нашего приложения.
+const electron = require('electron')
+const app = electron.app
 const Tray = electron.Tray
 const Menu = electron.Menu
 const ipc = electron.ipcMain
-const request = require('request')
 
-const dialog = electron.dialog
+const shell = electron.shell
 const globalShortcut = electron.globalShortcut
-const BrowserWindow = electron.BrowserWindow;  // Модуль создающий браузерное окно.
+const BrowserWindow = electron.BrowserWindow
 
-var mainWindow = null;
-var quickAddWindow = null;
+var mainWindow = null
+var quickAddWindow = null
 
-// Проверяем что все окна закрыты и закрываем приложение.
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
     app.quit();
@@ -21,14 +19,12 @@ app.on('window-all-closed', function() {
 
 let tray = null
 
-// Этот метод будет вызван когда Electron закончит инициализацию
-// и будет готов к созданию браузерных окон.
 app.on('ready', function() {
-  // Создаем окно браузера.
   createMainWindow()
   createTray()
+
   globalShortcut.register('CommandOrControl+Shift+Alt+N', function (event) {
-    createQuickAdd();
+    toggleQuickAddWindow()
   })
 
 
@@ -54,47 +50,58 @@ function createMainWindow() {
     mainWindow = null;
   });
 
+  mainWindow.on('close', function(e){
+    e.preventDefault()
+    mainWindow.hide()
+  })
+
 }
 
-function createQuickAdd () {
-  if( quickAddWindow == null ){
-      quickAddWindow = new BrowserWindow(
-        {
-          width: 540,
-          height: 80,
-          // webPreferences:
-          //   {
-          //     nodeIntegration: false
-          //   },
-          modal: true,
-          frame: false,
-          parent: mainWindow,
-        }
-      );
-      quickAddWindow.on('closed', function() {
-        if ( quickAddWindow != null ){
-          quickAddWindow = null;
-        }
-      })
+function createQuickAddWindow () {
+  quickAddWindow = new BrowserWindow(
+    {
+      width: 540,
+      height: 80,
+      modal: true,
+      frame: false,
+      parent: mainWindow,
+      alwaysOnTop: true,
     }
-    quickAddWindow.loadURL('file://' + __dirname + '/html/quickadd.html');
+  );
+
+  quickAddWindow.loadURL('file://' + __dirname + '/html/quickadd.html');
+  quickAddWindow.show()
+
+  ipc.on('create-text-note', function(event, text){
+    createTextNote(text)
+    // request('https://nimbus.everhelper.me/api/me', function (error, response, body) {
+    //   console.log('error:', error); // Print the error if one occurred
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   console.log('body:', body); // Print the HTML for the Google homepage.
+    // });
+    // дергаем апи для сохранения заметки
+    // https://nimbus.everhelper.me/api/notes/cXmYOJUTpzMWYKfB/text
+  });
+}
+
+function createTextNote(text){
+  console.log(text)
+}
+
+function toggleQuickAddWindow(){
+  if( quickAddWindow == null ){
+    createQuickAddWindow()
+  }else{
     quickAddWindow.show()
+  }
+}
 
-
-
-    ipc.on('create-new-note', function(event, store){
-      console.log(store)
-      if( quickAddWindow != null ){
-        quickAddWindow.hide()
-      }
-      request('https://nimbus.everhelper.me/api/me', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-      });
-      // дергаем апи для сохранения заметки
-      // https://nimbus.everhelper.me/api/notes/cXmYOJUTpzMWYKfB/text
-    });
+function toggleMainWindow(){
+  if( mainWindow == null ){
+    createMainWindow()
+  }else{
+    mainWindow.show()
+  }
 }
 
 function createTray () {
@@ -119,11 +126,17 @@ function createTray () {
     // tray.setContextMenu(contextMenu)
     tray = new Tray(__dirname + '/png/everhelper-1.png')
     const contextMenu = Menu.buildFromTemplate([
-      {label: 'Item1', type: 'radio'},
-      {label: 'Item2', type: 'radio'},
-      {label: 'Item3', type: 'radio', checked: true},
-      {label: 'Item4', type: 'radio'}
+      {label: 'Open Nimbus', click(){
+        if ( mainWindow == null ) { createWindow() }
+      }},
+      {label: 'Quickadd', click(){
+        toggleQuickAddWindow()
+      }},
+      {label: "Quit", click() { mainWindow.close() }},
+      {label: "GitHub", click() {
+        shell.openExternal('https://github.com/romkaspb/nimbustrone')
+      }}
     ])
-    tray.setToolTip('This is my application.')
+
     tray.setContextMenu(contextMenu)
 }
